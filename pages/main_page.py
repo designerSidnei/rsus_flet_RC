3525b1a7e7589ebb529f9aabea7940585afc2a52
Sidnei <sidneysantos861@gmail.com>
2024-08-23 20:33:24 -0300

    Changes in homepage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,8 @@
-from flet import Column, Row, Text, Page
+from flet import Column, Page, Row, Text
+
+# from datetime import datetime
+# import time
+
 from modules.load_config import config_read
 
 
@@ -12,13 +16,21 @@ class MainPage(Row):
                     value=self.user,
                     size=25,
                 )
+        # self.date_now = Text(size=20,)
+        # self.today_datetime()
 
     def build(self):
         return Column(
             [
                 self.user_text,
+                # self.date_now,
             ],
         )
+
+    # def today_datetime(self):
+    #    self.date_now.value = str(datetime.now())
+    #    time.sleep(1)
+    #    self.page.update()
 
     def congig_user(self):
         user = self.config["usuario"]
@@ -26,10 +38,11 @@ class MainPage(Row):
             return "Olá, Usuário!\nBem vindo!"
         else:
             return f"Olá, {user}!\nBem vindo!"
-            
-    
+
+
     def updater(self):
         self.config = config_read()
         self.user = self.congig_user()
         self.user_text.value = self.user
         self.page.update()
+
