3685bc749ac026a2a7f18f7d5f8126cc5f830ba1
Sidnei <sidneysantos861@gmail.com>
2024-08-24 09:52:46 -0300

    Adicionado data e tempo na página inicial

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
-from flet import Column, Page, Row, Text
+from flet import Column, Page, Row, Text, CrossAxisAlignment
 
-# from datetime import datetime
-# import time
+from datetime import datetime
+import asyncio
 
 from modules.load_config import config_read
 
@@ -13,24 +13,44 @@ class MainPage(Row):
         self.config = config_read()
         self.user = self.congig_user()
         self.user_text = Text(
-                    value=self.user,
-                    size=25,
-                )
-        # self.date_now = Text(size=20,)
-        # self.today_datetime()
+            value=self.user,
+            size=25,
+        )
+
+        self.past = datetime.now()
+        self.date_now = Text(value=datetime.now().strftime("%d/%m/%Y"), size=20,)
+        self.time_now = Text(size=20,)
+
+    def did_mount(self):
+        self.running = True
+        self.page.run_task(self.today_datetime)
+
+    # def will_unmount(self):
+    #     self.running = False
 
     def build(self):
         return Column(
-            [
+            horizontal_alignment=CrossAxisAlignment.CENTER,
+            controls=[
                 self.user_text,
-                # self.date_now,
+                self.date_now,
+                self.time_now,
             ],
         )
 
-    # def today_datetime(self):
-    #    self.date_now.value = str(datetime.now())
-    #    time.sleep(1)
-    #    self.page.update()
+    async def today_datetime(self):
+        while self.running:
+            if datetime.now() > self.past:
+                self.time_now.value = datetime.now().strftime("%H:%M:%S")
+            else:
+                self.time_now.value = str(self.past)
+            self.page.update()
+            try:
+                await asyncio.sleep(1)
+            except asyncio.exceptions.CancelledError:
+                break
+            except Exception:
+                print("")
 
     def congig_user(self):
         user = self.config["usuario"]
@@ -39,10 +59,8 @@ class MainPage(Row):
         else:
             return f"Olá, {user}!\nBem vindo!"
 
-
     def updater(self):
         self.config = config_read()
         self.user = self.congig_user()
         self.user_text.value = self.user
         self.page.update()
-
